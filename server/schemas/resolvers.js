df8520a14f5e9115e4da46a04c257ecf0bac8ebc
Daniel Bowen <djbowen95@gmail.com>
2022-06-02 19:05:48 +0100

    Resolver for addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,7 +13,14 @@ const resolvers = {
     },
   },
 
-
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+  },
 };
 
 module.exports = resolvers;
