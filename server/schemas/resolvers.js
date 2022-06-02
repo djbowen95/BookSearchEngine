fe80bc2aa14f6808675ec13bf329148eef25983a
Daniel Bowen <djbowen95@gmail.com>
2022-06-02 19:05:16 +0100

    Resolver for user query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,19 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const currentUser = await User.findOne({ _id: context.user._id });
+        return currentUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+
+};
 
 module.exports = resolvers;
