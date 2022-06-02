1edb6342d11f60ffa4413d37bcae72f12ee1304e
Daniel Bowen <djbowen95@gmail.com>
2022-06-02 16:41:54 +0100

    Define removeBook mutation in type schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,13 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
+        removeBook(bookId: String): User
     }
 `;
+
+// Need to add the saveBook mutation using an input type to handle many parameters
+// Look at how to do author / why that works
+// Should the User have a password - is in example but not in doc, maybe we don't want to be able to query this? 
+// Is bookID a string / check all scalars.
 
 module.exports = typeDefs;
