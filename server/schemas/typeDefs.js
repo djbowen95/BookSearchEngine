84f88159ab76862cdd7bd157f7e9a8f1d1f8bf51
Daniel Bowen <djbowen95@gmail.com>
2022-06-02 16:14:06 +0100

    Define type for Book queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,19 @@ const typeDefs = gql`
         bookCount: Int
         savedBooks: [Book]!
     }
+    
+    type Book {
+        bookId: String
+        authors: [Author]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Author {
+        name: String
+    }
 
 `;
 
