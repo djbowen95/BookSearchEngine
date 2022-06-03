76fb5759c09bc8213c566ea51e8bb3ffe7f74a24
Daniel Bowen <djbowen95@gmail.com>
2022-06-03 18:04:14 +0100

    Mutation util for saveBooks query

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation LogIn($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -24,3 +24,19 @@ export const ADD_USER = gql`
   }
 `;
 
+export const SAVE_BOOK = gql`
+  mutation SaveBook($book: bookInput) {
+    saveBook(bookData: book) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
